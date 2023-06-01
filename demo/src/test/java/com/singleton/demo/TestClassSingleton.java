ad471694f759efef843756220f42dcf2199f7fe9
MiRandria <hei.minosoa@gmail.com>
2023-06-01 11:42:07 +0300

    feat: add test

--- demo/src/test/java/com/singleton/demo/TestClassSingleton.java
+++ demo/src/test/java/com/singleton/demo/TestClassSingleton.java
@@ -5,11 +5,11 @@ import com.singleton.demo.classes.ClassSingleton;
 public class TestClassSingleton {
     public static void main(String[] args) {
         ClassSingleton singleton1 = ClassSingleton.getInstance();
-        System.out.println("Description: " + singleton1.getDescription());
+        System.out.println("Text: " + singleton1.getDescription());
 
         singleton1.setDescription("Updated description");
 
         ClassSingleton singleton2 = ClassSingleton.getInstance();
-        System.out.println("Description: " + singleton2.getDescription());
+        System.out.println("Text: " + singleton2.getDescription());
     }
 }
--- demo/src/test/java/com/singleton/demo/TestClassUniqueInstance.java
+++ demo/src/test/java/com/singleton/demo/TestClassUniqueInstance.java
@@ -5,11 +5,11 @@ import com.singleton.demo.classes.ClassUniqueInstance;
 public class TestClassUniqueInstance {
     public static void main(String[] args) {
         ClassUniqueInstance instance1 = ClassUniqueInstance.getInstance();
-        System.out.println("Enum: " + instance1.getClick());
+        System.out.println("enum: " + instance1.getClick());
 
         instance1.setClick(5);
 
         ClassUniqueInstance instance2 = ClassUniqueInstance.getInstance();
-        System.out.println("Enum: " + instance2.getClick());
+        System.out.println("enum: " + instance2.getClick());
     }
 }
